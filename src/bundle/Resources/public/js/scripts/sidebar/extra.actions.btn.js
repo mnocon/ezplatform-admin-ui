7fb6e6e00881243d4255f3fc5fd24aae3a738991
Piotr Nalepa <piotr.nalepa@ez.no>
2017-10-27 09:42:14 +0200

    in progress

--- src/bundle/Resources/public/js/scripts/sidebar/extra.actions.btn.js
+++ src/bundle/Resources/public/js/scripts/sidebar/extra.actions.btn.js
@@ -1,9 +1,21 @@
 (function () {
+    const CLASS_HIDDEN = 'ez-extra-actions--hidden';
     const btns = [...document.querySelectorAll('.ez-btn--extra-actions')];
+    const allActions = [...document.querySelectorAll('.ez-extra-actions')];
 
     btns.forEach(btn => {
-        const actions = btn.parentNode.querySelector('.ez-extra-actions');
+        const actions = allActions.find(item => item.dataset.btn === btn.dataset.actions);
 
-        btn.addEventListener('click', () => actions.classList.toggle('ez-extra-actions--hidden'), false);
+
+
+        btn.addEventListener('click', () => {
+            const btnRect = btn.getBoundingClientRect();
+
+            allActions.forEach(item => item.classList.add(CLASS_HIDDEN));
+
+            actions.classList.remove(CLASS_HIDDEN);
+            // actions.style.top = btnRect.top + 'px';
+            // actions.style.left = btnRect.left + 'px';
+        }, false);
     });
 })();
